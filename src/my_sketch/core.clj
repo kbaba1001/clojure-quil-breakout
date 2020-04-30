a46ea5fc5594bc078cfd1383019944357e377103
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-04-30 18:45:21 +0900

    game over

--- src/my_sketch/core.clj
+++ src/my_sketch/core.clj
@@ -3,43 +3,46 @@
             [quil.middleware :as m]))
 
 ; TODO
-; * ボールが底についたらゲームオーバー
-; * スタートボタンでゲーム開始
 ; * ブロックにボールが当たったらブロックを消す
 ; * ブロックが全て消えたらゲームクリア
 
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :rgb)
-  {:boll-x (/ (q/width) 2)
-   :boll-y (/ (q/height) 4)
+  {:gameover false
+   :boll-x (/ (q/width) 2)
+   :boll-y (/ (q/height) 2)
    :boll-dx +
    :boll-dy +
    :pad-x (/ (q/width) 2)
    :blocks (repeat 5 true)})
 
 (defn update-state [state]
-  (with-local-vars [a-state state]
-    (var-set a-state (update @a-state :boll-x (:boll-dx @a-state) 5))
-    (var-set a-state (update @a-state :boll-y (:boll-dy @a-state) 5))
-    (when (> (:boll-x @a-state) (q/width))
-      (var-set a-state (assoc @a-state :boll-dx -)))
-    (when (< (:boll-x @a-state) 0)
-      (var-set a-state (assoc @a-state :boll-dx +)))
-    (when (> (:boll-y @a-state) (q/height))
-      ; TODO ゲームオーバーにする
-      (var-set a-state (assoc @a-state :boll-dy -)))
-    (when (< (:boll-y @a-state) 0)
-      (var-set a-state (assoc @a-state :boll-dy +)))
-    (when (and (> (:boll-x @a-state) (:pad-x @a-state))
-               (< (+ (:boll-x @a-state) 16) (+ (:pad-x @a-state) 60))
-               (> (+ (:boll-y @a-state) 16) (- (q/height) 30)))
-      (var-set a-state (assoc @a-state :boll-dy -)))
-    @a-state))
+  (if (:gameover state)
+    state
+    (with-local-vars [a-state state]
+      (var-set a-state (update @a-state :boll-x (:boll-dx @a-state) 5))
+      (var-set a-state (update @a-state :boll-y (:boll-dy @a-state) 5))
+      (when (> (:boll-x @a-state) (q/width))
+        (var-set a-state (assoc @a-state :boll-dx -)))
+      (when (< (:boll-x @a-state) 0)
+        (var-set a-state (assoc @a-state :boll-dx +)))
+      (when (> (:boll-y @a-state) (q/height))
+        (var-set a-state (assoc @a-state :gameover true)))
+      (when (< (:boll-y @a-state) 0)
+        (var-set a-state (assoc @a-state :boll-dy +)))
+      (when (and (> (:boll-x @a-state) (:pad-x @a-state))
+                 (< (+ (:boll-x @a-state) 16) (+ (:pad-x @a-state) 60))
+                 (> (+ (:boll-y @a-state) 16) (- (q/height) 30)))
+        (var-set a-state (assoc @a-state :boll-dy -)))
+      @a-state)))
 
 (defn draw-state [state]
   (q/background 50)
   (q/fill 255)
+  (when (:gameover state)
+    (q/text-size 30)
+    (q/text "GAME OVER" 160 (/ (q/height) 2)))
   (let [bx (:boll-x state)
         by (:boll-y state)
         px (:pad-x state)
@@ -57,11 +60,15 @@
 (defn key-pressed [state event]
   (condp = (:key event)
     :left (if (> (:pad-x state) 0)
-            (update state :pad-x - 10)
+            (update state :pad-x - 12)
             state)
     :right (if (< (+ (:pad-x state) 60) (q/width))
-             (update state :pad-x + 10)
+             (update state :pad-x + 12)
              state)
+    :space (assoc state
+                  :gameover false
+                  :boll-x (/ (q/width) 2)
+                  :boll-y (/ (q/height) 2))
     state))
 
 (q/defsketch my-sketch
